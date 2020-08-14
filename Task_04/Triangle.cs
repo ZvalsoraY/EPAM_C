82abbe75ddb8c0302485b65d7822c670283b4cf6
ZvalsoraY <yazamail@yandex.ru>
2020-08-14 21:13:49 +0300

    XML comments were added

--- Task_04/Triangle.cs
+++ Task_04/Triangle.cs
@@ -4,12 +4,17 @@ using System.Text;
 
 namespace Task_04
 {
+    /// <summary>
+    /// Triangle class.
+    /// It takes 3 arguments, which are the lengths of its sides.
+    /// When creating an instance, the possibility of the existence
+    /// of a triangle with the specified parameters is automatically checked.
+    /// </summary>
     public class Triangle
     {
         private double a;
         private double b;
         private double c;
-
         private Triangle() { }
 
         public Triangle(double a, double b, double c)
@@ -33,31 +38,25 @@ namespace Task_04
         {
             get { return c; }
         }
-
-
-        //double a;
-        //double b;
-        //double c;
-        //public Triangle(double a, double b, double c)
-        //{
-        //    this.a = a;
-        //    this.b = b;
-        //    this.c = c;
-        //    if (PossibleTriangle(a, b, c))
-        //        throw new ArgumentException("Не существует треугольника с заданными параметрами");
-        //}
-
+        /// <summary>
+        /// Method for testing the possibility of the existence of a triangle with given parameters.
+        /// </summary>
         public bool PossibleTriangle(double a, double b, double c)
         {
             if (a + b > c && a + c > b && b + c > a) return true;
             else return false;
         }
-
+        /// <summary>
+        /// The method returns the perimeter of the triangle.
+        /// </summary>
         public double PerimeterTriangle(double a, double b, double c)
         {
             return a + b + c;
         }
-
+        /// <summary>
+        /// The method returns the square of the triangle.
+        /// The formula uses a semi-perimeter.
+        /// </summary>
         public double SquareTriangle(double a, double b, double c)
         {
             double p = PerimeterTriangle(a, b, c) / 2;
