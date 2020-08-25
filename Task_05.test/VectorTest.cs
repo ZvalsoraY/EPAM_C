c2047b98430f885a35285e158c837c2b0ce17281
ZvalsoraY <yazamail@yandex.ru>
2020-08-25 20:02:04 +0300

    xz

--- Task_05.test/VectorTest.cs
+++ Task_05.test/VectorTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
 using Task_05;
 
 namespace Task_05.test
@@ -22,10 +23,25 @@ namespace Task_05.test
             Vector vector1 = new Vector(3, 4, 5);
             Vector vector2 = new Vector(-3, -4, -5);
             Vector expectedVector = new Vector(0, 0, 0);
-            //Assert.AreEqual(vectorRes.ToString(), (vector1 + vector2).ToString());
-            Assert.That{ expectedVector,Is.EqualT}
-            Assert.That(expectedVector, Is.EqualTo(vector1 + vector2).AsCollection.Within(1.0E-7));
+            //Assert.AreEqual(expectedVector.ToString(), (vector1 + vector2).ToString());
+            //Assert.AreEqual(expectedVector, vector1 - vector2);
+            //CollectionAssert.AreEqual((System.Collections.ICollection)expectedVector, (System.Collections.ICollection)(vector1 + vector2));
+            //CollectionAssert.AreEqual((System.Collections.ICollection) expectedVector, (System.Collections.ICollection) (vector1 + vector2));
+            CollectionAssert.AreEqual((ICollection)expectedVector, (ICollection)(vector1 + vector2));
+            CollectionAssert.AreEqual(expectedVector, (vector1 + vector2));
+            //Assert.That(expectedVector, Is.EqualTo(vector1 + vector2).AsCollection.Within(1.0E-7));
             //            Assert.AreEqual(vectorRes, vector1 + vector2, "correct");
         }
+        //public void DifferenceVectorTest()
+        //{
+        //    Vector vector1 = new Vector(3, 4, 5);
+        //    Vector vector2 = new Vector(3, 4, 5);
+        //    Vector expectedVector = new Vector(0, 0, 0);
+        //    //Assert.AreEqual(expectedVector.ToString(), (vector1 + vector2).ToString());
+        //    Assert.AreEqual(expectedVector, vector1 - vector2);
+        //    CollectionAssert.AreEqual((System.Collections.ICollection)expectedVector, (System.Collections.ICollection)(vector1 - vector2));
+        //    //Assert.That(expectedVector, Is.EqualTo(vector1 + vector2).AsCollection.Within(1.0E-7));
+        //    //            Assert.AreEqual(vectorRes, vector1 + vector2, "correct");
+        //}
     }
 }
