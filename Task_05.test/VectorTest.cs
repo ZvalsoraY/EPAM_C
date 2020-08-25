e7e2b86e81c375adb68711fedf6b18b240b5e4db
ZvalsoraY <yazamail@yandex.ru>
2020-08-25 14:20:14 +0300

    test problem

--- Task_05.test/VectorTest.cs
+++ Task_05.test/VectorTest.cs
@@ -16,5 +16,16 @@ namespace Task_05.test
             Vector vector = new Vector(x, y, z);
             Assert.AreEqual(Math.Sqrt(x * x + y * y + z * z), vector.Length(), 0.001, "correct");
         }
+        [TestMethod]
+        public void SummVectorTest()
+        {
+            Vector vector1 = new Vector(3, 4, 5);
+            Vector vector2 = new Vector(-3, -4, -5);
+            Vector expectedVector = new Vector(0, 0, 0);
+            //Assert.AreEqual(vectorRes.ToString(), (vector1 + vector2).ToString());
+            Assert.That{ expectedVector,Is.EqualT}
+            Assert.That(expectedVector, Is.EqualTo(vector1 + vector2).AsCollection.Within(1.0E-7));
+            //            Assert.AreEqual(vectorRes, vector1 + vector2, "correct");
+        }
     }
 }
