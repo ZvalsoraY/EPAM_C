dd103d621d81de89dc17afb191bbf12da45430d4
ZvalsoraY <yazamail@yandex.ru>
2020-08-27 22:04:57 +0300

    tests done

--- Task_05.test/VectorTest.cs
+++ Task_05.test/VectorTest.cs
@@ -34,16 +34,46 @@ namespace Task_05.test
             //Assert.That(expectedVector, Is.EqualTo(vector1 + vector2).AsCollection.Within(1.0E-7));
             //            Assert.AreEqual(vectorRes, vector1 + vector2, "correct");
         }
-        //public void DifferenceVectorTest()
-        //{
-        //    Vector vector1 = new Vector(3, 4, 5);
-        //    Vector vector2 = new Vector(3, 4, 5);
-        //    Vector expectedVector = new Vector(0, 0, 0);
-        //    //Assert.AreEqual(expectedVector.ToString(), (vector1 + vector2).ToString());
-        //    Assert.AreEqual(expectedVector, vector1 - vector2);
-        //    CollectionAssert.AreEqual((System.Collections.ICollection)expectedVector, (System.Collections.ICollection)(vector1 - vector2));
-        //    //Assert.That(expectedVector, Is.EqualTo(vector1 + vector2).AsCollection.Within(1.0E-7));
-        //    //            Assert.AreEqual(vectorRes, vector1 + vector2, "correct");
-        //}
+        [TestMethod]
+        public void DifferenceVectorTest()
+        {
+            Vector vector1 = new Vector(3, 4, 5);
+            Vector vector2 = new Vector(3, 4, 5);
+            Vector expectedVector = new Vector(0, 0, 0);
+            Assert.AreEqual(true, (vector1 - vector2).Equals(expectedVector));
+        }
+        [TestMethod]
+        public void MultiplyVectorTest()
+        {
+            double x1 = 3;
+            double y1 = 4;
+            double z1 = 5;
+            Vector vector1 = new Vector(x1, y1, z1);
+            double x2 = 3;
+            double y2 = 4;
+            double z2 = 5;
+            Vector vector2 = new Vector(x2, y2, z2);
+            Assert.AreEqual(x1 * x2 + y1 * y2 + z1 * z2, vector1 * vector2, 0.001, "correct");
+        }
+        [TestMethod]
+        public void ConstMultiplyVectorTest()
+        {
+            double x = 3;
+            double y = 4;
+            double z = 5;
+            Vector vector = new Vector(x, y, z);
+            double n = 3;
+            Assert.AreEqual(x * n + y * n + z * n, vector * n, 0.001, "correct");
+        }
+        [TestMethod]
+        public void StringVectorTest()
+        {
+            double x = 3;
+            double y = 4;
+            double z = 5;
+            Vector vector = new Vector(x, y, z);
+            string expectedString = "(3,4,5)";
+            Assert.AreEqual(expectedString, vector.ToString(),"correct");
+        }
     }
 }
