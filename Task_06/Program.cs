96565c25d7f948f3d3b6a810f44f8d9f15400ba1
ZvalsoraY <yazamail@yandex.ru>
2020-09-02 07:20:29 +0300

    no comm

--- Task_06/Program.cs
+++ Task_06/Program.cs
@@ -22,9 +22,12 @@ namespace Task_06
             ProgramHelper test2 = new ProgramHelper();
             IConvertible test1pc = test1;
             IConvertible test2pc = test2;
-            Console.WriteLine(test1pc.ConvertToCSharp("test1 ConvertToCSharp"));
-            Console.WriteLine(test1pc.ConvertToVB("test1 ConvertToVB"));
-            Console.WriteLine(test2pc.);
+            var rezTest1ConvertToCSharp = test1pc.ConvertToCSharp("test1 ConvertToCSharp");
+            var rezTest1ConvertToVB = test1pc.ConvertToVB("test1 ConvertToVB");
+            //var rezTest2 = test1pc.
+            Console.WriteLine(rezTest1ConvertToCSharp);
+            Console.WriteLine(rezTest1ConvertToVB);
+            //Console.WriteLine(test2pc.);
 
 
         }
