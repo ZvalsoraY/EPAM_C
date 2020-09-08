eca961d5fad2d496a1804aa340b621cb8525724d
ZvalsoraY <yazamail@yandex.ru>
2020-09-08 07:20:39 +0300

    comm

--- Task_07/Matrix.cs
+++ Task_07/Matrix.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Task_07
 {
@@ -16,13 +14,13 @@ namespace Task_07
             this.matrixArray = matrixArray;
         }
         // метод расширения для получения количества строк матрицы
-        public static int RowsCount(double[,] matrix)
+        public long RowsCount(double[,] matrix)
         {
             return matrix.GetUpperBound(0) + 1;
         }
 
         // метод расширения для получения количества столбцов матрицы
-        public static int ColumnsCount(double[,] matrix)
+        public long ColumnsCount(double[,] matrix)
         {
             return matrix.GetUpperBound(1) + 1;
         }
@@ -32,11 +30,13 @@ namespace Task_07
         /// <returns>
         /// Outputting the matrix to the console.
         /// </returns>
-        public void OutputMatrix(double[,] matrix, long m, long n)
+        public void OutputMatrix(double[,] matrix)
         {
-            for (long i = 0; i < m; i++)
+            long matrixRow = RowsCount(matrix);
+            long matrixCol = ColumnsCount(matrix);
+            for (long i = 0; i < matrixCol; i++)
             {
-                for (long j = 0; j < n; j++)
+                for (long j = 0; j < matrixRow; j++)
                 {
                     Console.Write("{0}\t", matrix[i, j]);
                 }
@@ -51,14 +51,14 @@ namespace Task_07
         /// </returns>
         public double[,] AddMatrix(double[,] matrix1, double[,] matrix2)
         {
-            long sumMatrixRow = matrix1.GetUpperBound(0) + 1;
-            long sumMatrixCol = matrix1.GetUpperBound(1) + 1;
+            long sumMatrixRow = RowsCount(matrix1);
+            long sumMatrixCol = ColumnsCount(matrix1);
             double[,] sumMatrix = new double[sumMatrixRow, sumMatrixCol];
             //if (matrix1.Length != matrix2.Length)
             //    return null;
-            for (long i = 0; i < this.column; i++)
+            for (long i = 0; i < sumMatrixCol; i++)
             {
-                for (long j = 0; j < this.row; j++)
+                for (long j = 0; j < sumMatrixRow; j++)
                 {
                     sumMatrix[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
@@ -68,14 +68,14 @@ namespace Task_07
         //Метод разности двух матриц
         public double[,] SubMatrix(double[,] matrix1, double[,] matrix2)
         {
-            long subMatrixRow = matrix1.GetUpperBound(0) + 1;
-            long subMatrixCol = matrix1.GetUpperBound(1) + 1;
+            long subMatrixRow = RowsCount(matrix1);
+            long subMatrixCol = ColumnsCount(matrix1);
             double[,] subMatrix = new double[subMatrixRow, subMatrixCol];
             //if (matrix1.Length != matrix2.Length)
             //    return null;
-            for (long i = 0; i < this.column; i++)
+            for (long i = 0; i < subMatrixCol; i++)
             {
-                for (long j = 0; j < subMatrixCol; j++)
+                for (long j = 0; j < subMatrixRow; j++)
                 {
                     subMatrix[i, j] = matrix1[i, j] - matrix2[i, j];
                 }
@@ -83,8 +83,11 @@ namespace Task_07
             return subMatrix;
         }
         //Метод умножения матрицы на заданное число
-        public void FacMatrix(double[,] matrix, double factor) 
+        public double[,] FacMatrix(double[,] matrix, double factor) 
         {
+            //long facMatrixRow = RowsCount(matrix);
+            //long facMatrixCol = ColumnsCount(matrix);
+            //double[,] facMatrix = new double[facMatrixRow, facMatrixCol];
             for (long i = 0; i < matrix.GetUpperBound(1) + 1; i++)
             {
                 for (long j = 0; j < matrix.GetUpperBound(0) + 1; j++)
@@ -92,27 +95,28 @@ namespace Task_07
                     matrix[i, j] = factor * matrix[i, j];
                 }
             }
+            return matrix;
         }
         //перемножение матриц
         // метод для умножения матриц
-        static int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+        static long[,] MultiplicationMatrix(long[,] matrix1, long[,] matrix2)
         {
-            if (matrixA.ColumnsCount() != matrixB.RowsCount())
+            if (matrix1.ColumnsCount() != matrix2.RowsCount())
             {
                 throw new Exception("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
             }
 
-            var matrixC = new int[matrixA.RowsCount(), matrixB.ColumnsCount()];
+            var matrixC = new int[matrix1.RowsCount(), matrix2.ColumnsCount()];
 
-            for (var i = 0; i < matrixA.RowsCount(); i++)
+            for (var i = 0; i < matrix1.RowsCount(); i++)
             {
-                for (var j = 0; j < matrixB.ColumnsCount(); j++)
+                for (var j = 0; j < matrix2.ColumnsCount(); j++)
                 {
                     matrixC[i, j] = 0;
 
-                    for (var k = 0; k < matrixA.ColumnsCount(); k++)
+                    for (var k = 0; k < matrix1.ColumnsCount(); k++)
                     {
-                        matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                        matrixC[i, j] += matrix1[i, k] * matrix2[k, j];
                     }
                 }
             }
